54c97f17e28e67c1aa65d4a6f9940f35a8073f44
Alejandro <65319006+Ositobailador@users.noreply.github.com>
2021-01-27 19:31:55 -0800

    Update lab.js
    Editted js file for lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Assignment: Lab 5 - Data Types & Variables
+ * Author:    Alejandro and Gillian
+ * Created:   01.26.2021
+ *
+ * Licensed: MIT 2.0
+ **/
+
+// Define variables
+var make = "Ford";
+var model = "Taurus";
+var color = "blue";
+var year = 2004;
+var ownIt = true;
+
+// Calculate
+var currentYear = 2020;
+var age = currentYear - year;
+
+// Output
+document.writeln("Make: " + make.fontcolor("violet") + "<br>");
+document.writeln("Model: " + model.fontcolor("violet") + "<br>");
+document.writeln("Color: " + color.fontcolor("violet") + "<br>");
+document.writeln("Year: " + year.toString().fontcolor("violet") + "<br>");
+document.writeln("Age: " + age.toString().fontcolor("violet") + " years<br>".fontcolor("violet"));
